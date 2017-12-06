fb8e9a4ed563dd5550635ba6a8b679c196fb55c8
Peter Bonnett <Pete@Pete-B.co.uk>
2017-12-06 00:02:56 +0000

    Day 5

---
+++ src/main/java/uk/co/pete_b/advent/aoc2017/Day05.java
@@ -0,0 +1,55 @@
+package uk.co.pete_b.advent.aoc2017;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Day05 {
+
+    public static int findNoOfStepsPart1(final String input) {
+        int numSteps = 0;
+        int currPointer = 0;
+        int lastPointer = 0;
+
+        final Integer[] offsets = getOffsets(input);
+
+        while (currPointer < offsets.length) {
+            int offset = offsets[currPointer];
+            lastPointer += offset;
+            offsets[currPointer]++;
+            currPointer = lastPointer;
+
+            numSteps++;
+        }
+
+        return numSteps;
+    }
+
+    public static int findNoOfStepsPart2(final String input) {
+        int numSteps = 0;
+        int currPointer = 0;
+        int lastPointer = 0;
+
+        final Integer[] offsets = getOffsets(input);
+
+        while (currPointer < offsets.length) {
+            int offset = offsets[currPointer];
+            lastPointer += offset;
+            if (offset >= 3) {
+                offsets[currPointer]--;
+            } else {
+                offsets[currPointer]++;
+            }
+
+            currPointer = lastPointer;
+
+            numSteps++;
+        }
+
+        return numSteps;
+    }
+
+    private static Integer[] getOffsets(String input) {
+        return Arrays.stream(input.split("\r?\n")).map(Integer::parseInt).toArray(Integer[]::new);
+    }
+}
---
+++ src/test/java/uk/co/pete_b/advent/aoc2017/Day05Test.java
@@ -0,0 +1,33 @@
+package uk.co.pete_b.advent.aoc2017;
+
+import org.apache.commons.io.IOUtils;
+import org.junit.Assert;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class Day05Test {
+    private static final Logger LOGGER = LoggerFactory.getLogger(Day05Test.class);
+
+    @Test
+    public void testPart1() {
+        Assert.assertEquals(5, Day05.findNoOfStepsPart1("0\n3\n0\n1\n-3"));
+    }
+
+    @Test
+    public void testPart2() {
+        Assert.assertEquals(10, Day05.findNoOfStepsPart2("0\n3\n0\n1\n-3"));
+    }
+
+    @Test
+    public void getAnswer() throws IOException {
+        final InputStream offsetStream = getClass().getResourceAsStream("/puzzle-data/2017/day05");
+        final String offsets = IOUtils.toString(offsetStream, "UTF-8");
+
+        LOGGER.info("Part 1: " + Day05.findNoOfStepsPart1(offsets));
+        LOGGER.info("Part 2: " + Day05.findNoOfStepsPart2(offsets));
+    }
+}
