855866b8174bb899d3a6add3f6450bf1a2643c79
Peter Bonnett <Pete@Pete-B.co.uk>
2017-12-06 23:01:22 +0000

    Day 6

---
+++ src/main/java/uk/co/pete_b/advent/aoc2017/Day06.java
@@ -0,0 +1,79 @@
+package uk.co.pete_b.advent.aoc2017;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Day06 {
+
+    public static int numberTillInfiniteLoop(final String input) {
+        int numSteps = 0;
+
+        final int[] blocks = getInitialBytes(input);
+
+        final Set<List<Integer>> history = new HashSet<>();
+
+        List<Integer> converted = convertToBoxedList(blocks);
+        while (!history.contains(converted)) {
+            history.add(converted);
+            numSteps++;
+
+            int largestIndex = findLargestIndex(blocks);
+            int largestValue = blocks[largestIndex];
+            blocks[largestIndex] = 0;
+            for (int i=0; i<largestValue; i++) {
+                int nextIndex = (largestIndex + i + 1) % blocks.length;
+                blocks[nextIndex]++;
+            }
+
+            converted = convertToBoxedList(blocks);
+        }
+
+        return numSteps;
+    }
+
+    public static int cyclesFromInfiniteLoop(final String input) {
+        int numSteps = 0;
+
+        final int[] blocks = getInitialBytes(input);
+
+        final Map<List<Integer>, Integer> historyList = new HashMap<>();
+
+        List<Integer> converted = convertToBoxedList(blocks);
+        while (!historyList.containsKey(converted)) {
+            historyList.put(converted, ++numSteps);
+
+            int largestIndex = findLargestIndex(blocks);
+            int largestValue = blocks[largestIndex];
+            blocks[largestIndex] = 0;
+            for (int i=0; i<largestValue; i++) {
+                int nextIndex = (largestIndex + i + 1) % blocks.length;
+                blocks[nextIndex]++;
+            }
+
+            converted = convertToBoxedList(blocks);
+        }
+
+        return (numSteps + 1) - historyList.get(converted);
+    }
+
+    private static int[] getInitialBytes(String input) {
+        return Arrays.stream(input.split("\\s")).mapToInt(Integer::parseInt).toArray();
+    }
+
+    private static List<Integer> convertToBoxedList(final int[] input) {
+        return Arrays.stream(input).boxed().collect(Collectors.toList());
+    }
+
+    private static int findLargestIndex(final int[] input) {
+        int largestIndex = 0;
+        int largest = input[largestIndex];
+        for (int i = 1; i<input.length; i++) {
+            if (input[i] > largest) {
+                largest = input[i];
+                largestIndex = i;
+            }
+        }
+
+        return largestIndex;
+    }
+}
---
+++ src/test/java/uk/co/pete_b/advent/aoc2017/Day06Test.java
@@ -0,0 +1,33 @@
+package uk.co.pete_b.advent.aoc2017;
+
+import org.apache.commons.io.IOUtils;
+import org.junit.Assert;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class Day06Test {
+    private static final Logger LOGGER = LoggerFactory.getLogger(Day06Test.class);
+
+    @Test
+    public void testPart1() {
+        Assert.assertEquals(5, Day06.numberTillInfiniteLoop("0\t2\t7\t0"));
+    }
+
+    @Test
+    public void testPart2() {
+        Assert.assertEquals(4, Day06.cyclesFromInfiniteLoop("0\t2\t7\t0"));
+    }
+
+    @Test
+    public void getAnswer() throws IOException {
+        final InputStream blockStream = getClass().getResourceAsStream("/puzzle-data/2017/day06");
+        final String blocks = IOUtils.toString(blockStream, "UTF-8");
+
+        LOGGER.info("Part 1: " + Day06.numberTillInfiniteLoop(blocks));
+        LOGGER.info("Part 2: " + Day06.cyclesFromInfiniteLoop(blocks));
+    }
+}
