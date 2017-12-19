a4e3bd6b62d0240ac5d8cfcfb3c9a3372802846d
Peter Bonnett <Pete@Pete-B.co.uk>
2017-12-19 19:35:49 +0000

    Day 19

---
+++ src/main/java/uk/co/pete_b/advent/aoc2017/Day19.java
@@ -0,0 +1,122 @@
+package uk.co.pete_b.advent.aoc2017;
+
+public class Day19 {
+
+    public static class Answers {
+        private final String pathNavigated;
+        private final int numberOfSteps;
+
+        Answers(final String pathNavigated, final int numberOfSteps) {
+            this.pathNavigated = pathNavigated;
+            this.numberOfSteps = numberOfSteps;
+        }
+
+        public String getPathNavigated() {
+            return pathNavigated;
+        }
+
+        public int getNumberOfSteps() {
+            return numberOfSteps;
+        }
+    }
+
+    private enum Direction {
+        LEFT, UP, RIGHT, DOWN
+    }
+
+    public static Answers navigatePath(final String input) {
+        final StringBuilder path = new StringBuilder();
+        int numSteps = 0;
+
+        final String[] lines = input.split("\r?\n");
+        final char[][] grid = new char[lines.length][];
+        for (int i = 0; i < lines.length; i++) {
+            grid[i] = lines[i].toCharArray();
+        }
+
+        // find start point
+        int x = 0;
+        int y = 0;
+        Direction heading = Direction.DOWN;
+        for (int i = 0; i < grid[y].length; i++) {
+            if (grid[y][i] == '|') {
+                x = i;
+                break;
+            }
+        }
+
+        boolean foundEnd = false;
+        while (!foundEnd) {
+            numSteps++;
+            final char currSquare = grid[y][x];
+            switch (currSquare) {
+                case '|':
+                case '-': {
+                    if (heading == Direction.DOWN) {
+                        y++;
+                    } else if (heading == Direction.UP) {
+                        y--;
+                    } else if (heading == Direction.LEFT) {
+                        x--;
+                    } else if (heading == Direction.RIGHT) {
+                        x++;
+                    }
+                    break;
+                }
+                case '+': {
+                    if (heading == Direction.DOWN || heading == Direction.UP) {
+                        if (x == 0) {
+                            heading = Direction.RIGHT;
+                            x++;
+                        } else if (x == grid[y].length - 1) {
+                            heading = Direction.LEFT;
+                            x--;
+                        } else if (grid[y][x + 1] == ' ') {
+                            heading = Direction.LEFT;
+                            x--;
+                        } else {
+                            heading = Direction.RIGHT;
+                            x++;
+                        }
+                    } else {
+                        if (y == 0) {
+                            heading = Direction.DOWN;
+                            y++;
+                        } else if (y == grid.length - 1) {
+                            heading = Direction.UP;
+                            y--;
+                        } else if (grid[y + 1][x] == ' ') {
+                            heading = Direction.UP;
+                            y--;
+                        } else {
+                            heading = Direction.DOWN;
+                            y++;
+                        }
+                    }
+                    break;
+                }
+                case ' ': {
+                    // Deduct the last step & exit
+                    foundEnd = true;
+                    numSteps--;
+                    break;
+                }
+                default: {
+                    path.append(currSquare);
+                    if (heading == Direction.DOWN) {
+                        y++;
+                    } else if (heading == Direction.UP) {
+                        y--;
+                    } else if (heading == Direction.LEFT) {
+                        x--;
+                    } else if (heading == Direction.RIGHT) {
+                        x++;
+                    }
+                    break;
+                }
+            }
+        }
+
+        return new Answers(path.toString(), numSteps);
+    }
+}
---
+++ src/test/java/uk/co/pete_b/advent/aoc2017/Day19Test.java
@@ -0,0 +1,39 @@
+package uk.co.pete_b.advent.aoc2017;
+
+import org.apache.commons.io.IOUtils;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.nio.charset.Charset;
+
+import static org.junit.Assert.assertEquals;
+
+public class Day19Test {
+    private static final Logger LOGGER = LoggerFactory.getLogger(Day19Test.class);
+
+    private static final String SAMPLE_INPUT = "     |          \n" +
+            "     |  +--+    \n" +
+            "     A  |  C    \n" +
+            " F---|----E|--+ \n" +
+            "     |  |  |  D \n" +
+            "     +B-+  +--+ \n";
+
+    @Test
+    public void testExamplesPart1() {
+        assertEquals("ABCDEF", Day19.navigatePath(SAMPLE_INPUT).getPathNavigated());
+    }
+
+    @Test
+    public void testExamplesPart2() {
+        assertEquals(38, Day19.navigatePath(SAMPLE_INPUT).getNumberOfSteps());
+    }
+
+    @Test
+    public void getAnswers() throws IOException {
+        final Day19.Answers answer = Day19.navigatePath(IOUtils.toString(getClass().getResourceAsStream("/puzzle-data/2017/day19"), Charset.defaultCharset()));
+        LOGGER.info("Part 1: " + answer.getPathNavigated());
+        LOGGER.info("Part 2: " + answer.getNumberOfSteps());
+    }
+}
